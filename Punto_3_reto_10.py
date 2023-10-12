29f1c5f35ceed1389d91c257f102259d24d1d0c5
Cate1911 <141857246+Cate1911@users.noreply.github.com>
2023-10-11 21:37:13 -0500

    Add files via upload

---
+++ Punto_3_reto_10.py
@@ -0,0 +1,3 @@
+#Algoritmo que deje al final de un arreglo de números todos los ceros que aparezcan en dicho arreglo
+arreglo_x = [10, 9, 0, 5, 6, 5, 7, 8, 34, 34, 25, 767, 456, 77, 88, 0, 44, 0, 9999, 104, 1999, 0]
+print(arreglo_x.count(0))
